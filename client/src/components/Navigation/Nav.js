10bbfe83ceae68840858f02d0178c64beae3091f
Artur <artur.kaluza@almservices.pl>
2018-06-26 16:02:26 +0200

    Check pull request

--- client/src/components/Navigation/Nav.js
+++ client/src/components/Navigation/Nav.js
@@ -6,12 +6,19 @@ const Navigation = () => (
   <nav>
     <div className="nav">
       <h2 className='nav__heading'>Main Menu</h2>
-        <ul className='nav__list'>
-          <NavLink to="/" className="nav__list-item" activeClassName="is-active" exact={true}>Home</NavLink>
-          <NavLink to="/cameras" className="nav__list-item" activeClassName="is-active">Cameras</NavLink>
-          <NavLink to="/tv" className="nav__list-item" activeClassName="is-active">TVs</NavLink>
-          <NavLink to="/phones" className="nav__list-item" activeClassName="is-active">Phones</NavLink>
-        </ul>
+      <ul className='nav__list'>
+        <NavLink to="/" className="nav__list-item" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/cameras" className="nav__list-item" activeClassName="is-active">Cameras</NavLink>
+        <NavLink to="/tv" className="nav__list-item" activeClassName="is-active">TVs</NavLink>
+        <NavLink to="/phones" className="nav__list-item" activeClassName="is-active">Phones</NavLink>
+      </ul>
+      <h2 className='nav__heading'>Item per page</h2>
+      <ul className='nav__list'>
+        <li className='nav__list-item'>3</li>
+        <li className='nav__list-item'>5</li>
+        <li className='nav__list-item'>7</li>
+        <li className='nav__list-item'>10</li>
+      </ul>
     </div>
   </nav>
 )
