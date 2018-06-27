b075f5a675e4523b80a4e0be32aa9882bf194fc2
Artur <artur.kaluza@almservices.pl>
2018-06-27 15:42:45 +0200

    Add load more btn

--- client/src/components/Cameras/Cameras.js
+++ client/src/components/Cameras/Cameras.js
@@ -9,13 +9,16 @@ class Cameras extends Component {
   constructor() {
     super();
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.paginationNext = this.paginationNext.bind(this);
     this.state = {
       cameras: [
         // {name: 'item1', amount: '20', desc: 'amazing item 1', price: 19.99}, 
         // {name: 'item100', amount: '520', desc: 'amazing item 100', price: 219.99},
         // {name: 'item100', amount: '520', desc: 'amazing item 100', price: 219.99}
       ],
-      inputVal: ''
+      inputVal: '',
+      currentPage: 1,
+      pages: null
     }
   }
   
@@ -29,8 +32,27 @@ class Cameras extends Component {
   componentDidMount() {
     fetch('http://localhost:3000/product/camera?page=1&limit=4')
      .then(res => res.json())
-     .then(data => this.setState({cameras: data.docs}))
+     .then(data => {
+      this.setState({
+        cameras: data.docs,
+        currentPage: data.page,
+        pages: data.pages
+      })
+      console.log(this.state.pages);
+    })
      .catch(e => console.log(e));
+  }
+
+  paginationNext() {
+    const nextPage = this.state.currentPage +1;
+    
+    fetch(`http://localhost:3000/product/camera?page=${nextPage}&limit=4`)
+      .then(res => res.json())
+      .then(data => this.setState({
+          cameras: data.docs,
+          currentPage: data.page,
+          pages: data.pages}))
+      .catch(e => console.log(e))
   }
 
   render() {
@@ -52,6 +74,8 @@ class Cameras extends Component {
 
             />)}
           </ul>
+          {}
+          {(this.state.pages && !(this.state.currentPage === this.state.pages) ) && <button className='next__btn' onClick={this.paginationNext} >Load more...</button>}
         </div>
       </div>
     )
