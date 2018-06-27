4943ab27896f759fa4c45d7800789a0b8c3d041b
Artur <artur.kaluza@almservices.pl>
2018-06-27 12:24:42 +0200

    add callback to searchBar

--- client/src/components/Cameras/Cameras.js
+++ client/src/components/Cameras/Cameras.js
@@ -7,26 +7,35 @@ import Camera from '../Camera/Camera';
 class Cameras extends Component {
   constructor() {
     super();
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
       cameras: [
-        {name: 'item1', amount: '20', desc: 'amazing item 1', price: 19.99}, 
-        {name: 'item100', amount: '520', desc: 'amazing item 100', price: 219.99},
-        {name: 'item100', amount: '520', desc: 'amazing item 100', price: 219.99}
-      ]
+        // {name: 'item1', amount: '20', desc: 'amazing item 1', price: 19.99}, 
+        // {name: 'item100', amount: '520', desc: 'amazing item 100', price: 219.99},
+        // {name: 'item100', amount: '520', desc: 'amazing item 100', price: 219.99}
+      ],
+      inputVal: ''
     }
   }
   
-   componentDidMount() {
-     fetch('http://localhost:3000/product/camera?page=1&limit=4')
-      .then(res => res.json())
-      .then(data => this.setState({cameras: data.docs}))
-      .catch(e => console.log(e));
-   }
+  handleInputChange(e) {
+    console.log('e.target:', e.target.value);
+    this.setState({inputVal: e.target.value});
+    console.log('value:', this.state.inputVal);
+  }
+
+  // fetch data from DB
+  componentDidMount() {
+    fetch('http://localhost:3000/product/camera?page=1&limit=4')
+     .then(res => res.json())
+     .then(data => this.setState({cameras: data.docs}))
+     .catch(e => console.log(e));
+  }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearch={this.handleInputChange}/>
         <div className='cameras'>
           <h3 className='cameras__title'>Cameras</h3>
           <ul>
--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -3,13 +3,14 @@ import style from './SearchBar.scss';
 
 class SearchBar extends Component {
   constructor() {
-    super()
+    super();
   }
+
   render() {
     return (
       <div className='searchBar'>
         <h3 className='searchBar__text'>Online Store</h3>
-        <input className='searchBar__input' type='text' placeholder='Search...' />        
+        <input className='searchBar__input' type='text' placeholder='Search...' onKeyUp={this.props.onSearch} />        
       </div>
     )
   }
